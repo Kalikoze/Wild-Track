0fd3c7929659298e8d0b1b0a5865605fca5df0cb
Travis Rollins <kalikoze@gmail.com>
2024-05-17 19:58:47 -0700

    Initial commit and add custom welcome page with styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,8 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      boxShadow: {
+        'dark-lg': '0 10px 15px -3px rgba(0, 0, 0, 0.7), 0 4px 6px -2px rgba(0, 0, 0, 0.6)',
       },
     },
   },
